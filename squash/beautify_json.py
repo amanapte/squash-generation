b4c4499efeec68527fff477cfa5c9b4d19d05e5b
Aman Apte <36478299+amanapte@users.noreply.github.com>
2020-06-25 14:56:54 +0530

    Update beautify_json.py

--- squash/beautify_json.py
+++ squash/beautify_json.py
@@ -1,11 +1,11 @@
 import simplejson as json
 
-json_list = [ "/final/Custom.json",
-                "/temp/Custom/final_qa_set.json", 
-                "/temp/Custom/generated_questions.json",
-                "/temp/Custom/nbest_predictions.json", 
-                "/temp/Custom/null_odds.json",
-                "/temp/Custom/predictions.json" ]
+json_list = [ "./final/Custom.json",
+                "./temp/Custom/final_qa_set.json", 
+                "./temp/Custom/generated_questions.json",
+                "./temp/Custom/nbest_predictions.json", 
+                "./temp/Custom/null_odds.json",
+                "./temp/Custom/predictions.json" ]
 
 for i in json_list:
     with open(i,"a") as f:
