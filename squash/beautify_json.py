919e3fda61498e899e5547091898f999a7269def
Aman Apte <36478299+amanapte@users.noreply.github.com>
2020-06-25 13:43:30 +0530

    Create beautify_json.py

---
+++ squash/beautify_json.py
@@ -0,0 +1,17 @@
+import simplejson as json
+
+json_list = [ "/final/Custom.json",
+                "/temp/Custom/final_qa_set.json", 
+                "/temp/Custom/generated_questions.json", 
+                "/temp/Custom/metadata.json",
+                "/temp/Custom/nbest_predictions.json", 
+                "/temp/Custom/null_odds.json",
+                "/temp/Custom/predictions.json" ]
+
+for i in json_list:
+    with open(i,"a") as f:
+        obj = json.load(f)
+        f.close()     
+    outfile = open(i, "w")
+    outfile.write(json.dumps(obj, indent=4, sort_keys=True))
+    outfile.close()
